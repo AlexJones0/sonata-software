56c3dc1671392a4710ffa432d5aa28d50f092a44
Alex Jones <alex.jones@lowrisc.org>
2024-11-12 12:32:32 +0000

    Add LED matrix to the proximity demo example

--- examples/all/proximity_sensor_example.cc
+++ examples/all/proximity_sensor_example.cc
@@ -9,6 +9,7 @@
 #include <debug.hh>
 #include <platform-i2c.hh>
 #include <platform-rgbctrl.hh>
+#include "../../libraries/sense_hat.hh"
 #include <thread.h>
 
 const uint8_t ApdS9960Enable = 0x80;
@@ -19,6 +20,8 @@ const uint8_t ApdS9960Pdata  = 0x9C;
 
 const uint8_t ApdS9960IdExp      = 0xAB;
 const uint8_t ApdS9960I2cAddress = 0x39;
+
+#define SENSE_HAT_AVAILABLE (true)
 
 /// Expose debugging features unconditionally for this compartment.
 using Debug = ConditionalDebug<true, "proximity sensor example">;
@@ -110,27 +113,42 @@ static uint8_t read_proximity_sensor(Mmio<OpenTitanI2c> i2c)
 
 [[noreturn]] void __cheri_compartment("proximity_sensor_example") run()
 {
+	// Initialise the Sense HAT in case we want to use it in this demo
+	constexpr SenseHat::Colour OffColour = {.red = 25, .green = 25, .blue = 25};
+	auto sense_hat = SenseHat();
+
 	auto i2cSetup = [](Mmio<OpenTitanI2c> i2c) {
 		i2c->reset_fifos();
 		i2c->host_mode_set();
 		i2c->speed_set(1);
 	};
 
-	auto i2c1 = MMIO_CAPABILITY(OpenTitanI2c, i2c1);
-	i2cSetup(i2c1);
+	auto i2c0 = MMIO_CAPABILITY(OpenTitanI2c, i2c0);
+	i2cSetup(i2c0);
 	uint8_t addr;
 
 	auto rgbled = MMIO_CAPABILITY(SonataRgbLedController, rgbled);
 
-	setup_proximity_sensor(i2c1, ApdS9960I2cAddress);
+	setup_proximity_sensor(i2c0, ApdS9960I2cAddress);
 
 	while (true)
 	{
-		uint8_t prox = read_proximity_sensor(i2c1);
+		uint8_t prox = read_proximity_sensor(i2c0);
 		Debug::log("Proximity is {}\r", prox);
 		rgbled->rgb(SonataRgbLed::Led0, ((prox) >> 3), 0, 0);
 		rgbled->rgb(SonataRgbLed::Led1, 0, (255 - prox) >> 3, 0);
 		rgbled->update();
+
+		if (SENSE_HAT_AVAILABLE) {
+			SenseHat::Colour fb[64] = {OffColour};
+			uint8_t filled = static_cast<uint8_t>(static_cast<uint64_t>(prox) * 64 / 255);
+			filled = (filled > 64) ? 64 : filled;
+			for (uint8_t i = 0; i < filled; i++) {
+				uint8_t red = static_cast<uint8_t>(static_cast<uint64_t>(i) * SenseHat::Colour::MaxRedValue / 64);
+				fb[i] = (SenseHat::Colour){.red = red, .green = 0, .blue = 0};
+			}
+			sense_hat.set_pixels(fb);
+		}
 
 		thread_millisecond_wait(100);
 	}
