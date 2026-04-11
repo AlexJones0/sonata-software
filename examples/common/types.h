9147f6faf70ae16012729fdc247b7a1063470410
Douglas Reis <doreis@lowrisc.org>
2026-04-11 15:02:01 +0100

    [automotive] Refactor build and create a legacy driver lib

---
+++ examples/common/types.h
@@ -0,0 +1,35 @@
+#pragma once
+// Constants.
+enum
+{
+	// Pin out mapping.
+	LcdCsPin = 0,
+	LcdDcPin,
+	LcdRstPin,
+	LcdMosiPin,
+	LcdSclkPin,
+	// Spi clock rate.
+	LcdSpiSpeedHz = 5 * 100 * 1000,
+};
+
+enum
+{
+	RGBColorBlack      = 0x000000,
+	RGBColorWhite      = 0xFFFFFF,
+	RGBColorBlue       = 0x0000FF,
+	RGBColorGreen      = 0x00FF00,
+	RGBColorRed        = 0xFF0000,
+	RGBColorLightGrey  = 0xAAAAAA,
+	RGBColorGrey       = 0xCCCCCC,
+	RGBColorDarkGrey   = 0xA0A0A0,
+	RGBColorDarkerGrey = 0x808080,
+};
+
+// Fonts available for LCD rendering
+typedef enum LcdFont
+{
+	M3x6_16pt,          // NOLINT
+	M5x7_16pt,          // NOLINT
+	LucidaConsole_10pt, // NOLINT
+	LucidaConsole_12pt, // NOLINT
+} LcdFont;
